3725a2d4c71d9c38981234e20b95b9e00b959b49
Sam Gwydir <sam@samgwydir.com>
2015-04-12 02:00:13 -0500

    add search paths

--- share/cmake/FindGMP.cmake
+++ share/cmake/FindGMP.cmake
@@ -1,22 +1,21 @@
-# FindGMP.cmake
-# Try to find the GMP librairies
-#  GMP_FOUND - system has GMP lib
-#  GMP_INCLUDE_DIR - the GMP include directory
-#  GMP_LIBRARIES - Libraries needed to use GMP
-
 # Copyright (c) 2006, Laurent Montel, <montel@kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# ﻿  PATHS ${GMP_PREFIX}/include /usr/include /usr/local/include)
+# message(${GMP_INCLUDE_DIR})
+
+# find_library(GMP_LIBRARY gmp 
+# ﻿  PATHS ${GMP_PREFIX}/lib /usr/lib /usr/local/lib)
 
 if (GMP_INCLUDE_DIR AND GMP_LIBRARIES)
   # Already in cache, be silent
   set(GMP_FIND_QUIETLY TRUE)
 endif (GMP_INCLUDE_DIR AND GMP_LIBRARIES)
 
-find_path(GMP_INCLUDE_DIR NAMES gmp.h )
-find_library(GMP_LIBRARIES NAMES gmp libgmp )
-find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx )
+find_path(GMP_INCLUDE_DIR NAMES gmp.h PATHS /usr/include /usr/local/include )
+find_library(GMP_LIBRARIES NAMES gmp libgmp /usr/lib /usr/local/lib )
+find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx /usr/lib /usr/local/lib )
 MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMPXX_LIBRARIES} )
 
 include(FindPackageHandleStandardArgs)

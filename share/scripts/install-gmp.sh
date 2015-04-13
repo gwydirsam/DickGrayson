781d7477d5de7f14b9540aea0d89724dc893bf91
Sam Gwydir <sam@samgwydir.com>
2015-04-12 19:06:18 -0500

    add install-gmp script

---
+++ share/scripts/install-gmp.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/.tmp
+
+URL=ftp://ftp.gmplib.org/pub/gmp/gmp-6.0.0a.tar.bz2
+TMP_DIR=${HOME}/.tmp
+INSTALL_PREFIX=${HOME}/local/$(uname)
+
+wget ${URL} -O ${TMP_DIR}/`basename $URL`
+
+tar -xjf ${TMP_DIR}/`basename $URL`
+
+mkdir -p ${INSTALL_PREFIX}
+
+# configure
+${TMP_DIR}/`basename $URL a.tar.bz2`/configure \
+          --prefix=${INSTALL_PREFIX} --enable-fat --enable-cxx
+
+# build
+make -C ${TMP_DIR}/`basename $URL a.tar.bz2`
+
+# check
+make -C ${TMP_DIR}/`basename $URL a.tar.bz2` check
+
+# install
+make -C ${TMP_DIR}/`basename $URL a.tar.bz2` install
+

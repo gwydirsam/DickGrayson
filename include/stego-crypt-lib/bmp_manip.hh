4d0e667eb7d369b2e8b008ad6f6c390dcb5ead27
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-14 02:10:07 -0500

    Test BmpOpen now passes

--- include/stego-crypt-lib/bmp_manip.hh
+++ include/stego-crypt-lib/bmp_manip.hh
@@ -2,12 +2,18 @@
 #define BMP_MANIP_HH
 
 #include <string>
+#include <fstream>
+#include <sstream>
 #include <iostream>
 
 typedef std::string bmp_t;
 
 bmp_t open_bmp(const std::string& fname) {
-  return bmp_t();
+  std::ifstream ifs(fname, std::ios::binary);
+  std::ostringstream ost;
+  ost << ifs.rdbuf();
+  bmp_t bmp(ost.str());
+  return bmp;
 }
 
 bool is_bmp_valid(const bmp_t& bmp) {

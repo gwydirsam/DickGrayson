8114ed04f3c08606036c06a324d30d555354b6da
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-14 06:22:03 -0500

    Merged develop, moved test.bmp into dg-img

--- test/dgimg/dgimg-test.cc
+++ test/dgimg/dgimg-test.cc
@@ -5,13 +5,15 @@
 #include <sstream>
 #include <dgimg/dgimg.hh>
 
+static std::string bmp_file = "test/dgimg/test.bmp";
+
 TEST(imageStub, oneEqualsone) {
   int one = 1;
   EXPECT_EQ(1, one);
 }
 
 TEST(BmpTests, Validation) {
-  std::ifstream ifs("test/stego-crypt/test.bmp", std::ios::binary);
+  std::ifstream ifs(bmp_file);
   std::ostringstream ost;
   ost << ifs.rdbuf();
   std::string bmp(ost.str());
@@ -19,23 +21,23 @@ TEST(BmpTests, Validation) {
 }
 
 TEST(BmpTests, Open) {
-  bmp_t bmp = open_bmp("test/stego-crypt/test.bmp");
+  bmp_t bmp = open_bmp(bmp_file);
   EXPECT_EQ(true, is_bmp_valid(bmp));
 }
 
 TEST(BmpTests, BitsPerPixel) {
-  bmp_t bmp = open_bmp("test/stego-crypt/test.bmp");
+  bmp_t bmp = open_bmp(bmp_file);
   EXPECT_EQ(8, bmp_bits_per_pixel(bmp));
 }
 
 TEST(BmpTests, Dimensions) {
-  bmp_t bmp = open_bmp("test/stego-crypt/test.bmp");
+  bmp_t bmp = open_bmp(bmp_file);
   EXPECT_EQ(512, bmp_width(bmp));
   EXPECT_EQ(512, bmp_height(bmp));
 }
 
 TEST(BmpTests, ImageOffset) {
-  bmp_t bmp = open_bmp("test/stego-crypt/test.bmp");
+  bmp_t bmp = open_bmp(bmp_file);
   unsigned offset = bmp_image_offset(bmp);
   // test.bmp is 8-bit and is 512*512
   EXPECT_EQ(512*512, bmp.size() - offset);

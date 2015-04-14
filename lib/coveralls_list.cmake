88706a2387fd91c34b54767af64228f36e03cbac
Sam Gwydir <sam@samgwydir.com>
2015-04-14 18:19:51 -0500

    get travis username and uname

--- lib/coveralls_list.cmake
+++ lib/coveralls_list.cmake
@@ -1,13 +1,15 @@
-if (COVERALLS)
-  list(APPEND COVERAGE_SRCS
-    ${PROJECT_SOURCE_DIR}/lib/${TYPE_LIB}/dgtype.cc
-    ${PROJECT_SOURCE_DIR}/lib/${IMAGE_LIB}/dgimg.cc
-    ${PROJECT_SOURCE_DIR}/lib/${CRYPTO_LIB}/dginteger.cc
-    ${PROJECT_SOURCE_DIR}/lib/${CRYPTO_LIB}/dgrandominteger.cc
-    ${PROJECT_SOURCE_DIR}/lib/${CRYPTO_LIB}/dgrandomprime.cc
-    ${PROJECT_SOURCE_DIR}/lib/rsa-crypt-lib/rsa-crypt-lib.cc
-    ${PROJECT_SOURCE_DIR}/lib/rsa-attack-lib/rsa-attack-lib.cc
-    ${PROJECT_SOURCE_DIR}/lib/stego-crypt-lib/stego-crypt-lib.cc
-    ${PROJECT_SOURCE_DIR}/lib/stego-attack-lib/stego-attack-lib.cc
-    )
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+  if (COVERALLS)
+    list(APPEND COVERAGE_SRCS
+      ${PROJECT_SOURCE_DIR}/lib/${TYPE_LIB}/dgtype.cc
+      ${PROJECT_SOURCE_DIR}/lib/${IMAGE_LIB}/dgimg.cc
+      ${PROJECT_SOURCE_DIR}/lib/${CRYPTO_LIB}/dginteger.cc
+      ${PROJECT_SOURCE_DIR}/lib/${CRYPTO_LIB}/dgrandominteger.cc
+      ${PROJECT_SOURCE_DIR}/lib/${CRYPTO_LIB}/dgrandomprime.cc
+      ${PROJECT_SOURCE_DIR}/lib/rsa-crypt-lib/rsa-crypt-lib.cc
+      ${PROJECT_SOURCE_DIR}/lib/rsa-attack-lib/rsa-attack-lib.cc
+      ${PROJECT_SOURCE_DIR}/lib/stego-crypt-lib/stego-crypt-lib.cc
+      ${PROJECT_SOURCE_DIR}/lib/stego-attack-lib/stego-attack-lib.cc
+      )
+  endif()
 endif()

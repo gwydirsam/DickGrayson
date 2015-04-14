60a9da9242ffbb815d3e323a8c09442cfb1c5d69
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-14 00:46:31 -0500

    Revert "pass bits test"
    This reverts commit 4bf9a57fc7909442c5e3f87e92e15bdbe94f706e.

--- test/dgcrypto/dgcrypto-test.cc
+++ test/dgcrypto/dgcrypto-test.cc
@@ -5,6 +5,6 @@
 TEST(DGCryptoPrime, CreatePrimeKBits) {
   // create a prime with 16u (unsigned int 16) bits
   DGPrime sixteen(16u);
-  EXPECT_GE(16u, sixteen.bits());
+  EXPECT_GT(8u, sixteen.bits());
 }
 

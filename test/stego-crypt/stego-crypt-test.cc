b8b41086ce1695eae4128423c619cbf8a049f139
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-14 00:39:42 -0500

    Wrote failing test for bmp validation

--- test/stego-crypt/stego-crypt-test.cc
+++ test/stego-crypt/stego-crypt-test.cc
@@ -1,7 +1,17 @@
 #include <gtest/gtest.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "stego-crypt-lib/bmp_manip.h"
 
 TEST(StegStub, oneEqualsone) {
   int one = 1;
   EXPECT_EQ(1, one);
 }
 
+TEST(BMPTests, BMPValidation) {
+  std::ifstream ifs("test.png", std::ios::binary);
+  std::string bmp;
+  ifs >> bmp;
+  EXPECT_EQ(true, is_bmp_valid(bmp));
+}

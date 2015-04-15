42f68a994ffa942b43d388e28601bc85dc0863da
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-14 23:33:52 -0500

    Bmp data tests all fail

--- lib/dgimg/bmp_data.cc
+++ lib/dgimg/bmp_data.cc
@@ -4,52 +4,31 @@ namespace DG {
 namespace Image {
 
 int BMP_data::bytes_to_int(const std::string& bytes) const {
-  int ret;
-  if (bytes.length() == 2) {
-    short temp = *(short*)bytes.c_str();
-    ret = temp;
-  } else {
-    ret = *(int*)bytes.c_str();
-  }
-  return ret;
+  return 0;
 }
 
 void BMP_data::open(const std::string& fname) {
-  std::ifstream ifs(fname, std::ios::binary);
-  std::ostringstream ost;
-  ost << ifs.rdbuf();
-  std::string bytes(ost.str());
-  data = bytes;
 }
 
 // returns the offset at which the pixel array begins (The data after the BMP header info)
 unsigned BMP_data::image_offset() const {
-  std::string bytes = data.substr(0xa, 4);
-  int offset = bytes_to_int(bytes);
-  return offset;
+  return 0;
 }
 
 unsigned BMP_data::bits_per_pixel() const {
-  std::string bytes = data.substr(0x1c, 2);
-  int bpp = bytes_to_int(bytes);
-  return bpp;
+  return 0;
 }
 
 int BMP_data::width() const {
-  std::string bytes = data.substr(0x12, 4);
-  int w = bytes_to_int(bytes);
-  return w;
+  return 0;
 }
 
 int BMP_data::height() const {
-  std::string bytes = data.substr(0x16, 4);
-  int h = bytes_to_int(bytes);
-  return h;
+  return 0;
 }
 
 bool BMP_data::is_valid() const {
-  std::string validation = data.substr(0, 2);
-  return validation == "BM";
+  return false;
 }
 }
 }

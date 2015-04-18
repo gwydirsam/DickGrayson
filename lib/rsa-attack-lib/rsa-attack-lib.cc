e32e2df1accfb1a6c1ee77f691eea416e90dc8a2
Sam Gwydir <sam@samgwydir.com>
2015-04-18 13:23:11 -0500

    commented and fixed exceptions rsa-attack-lib

--- lib/rsa-attack-lib/rsa-attack-lib.cc
+++ lib/rsa-attack-lib/rsa-attack-lib.cc
@@ -1,108 +1,161 @@
-#include <iostream>
-#include <limits>
-#include <string>
-#include <cassert>
 #include "rsa-attack-lib.hh"
-#include <dgcrypto/dgrandomprime.hh>
-using namespace std;
 
-// ported here from dg_randomprime until Rafa fixes..
-bool is_prime(mpz_class value) {
-  int k = 1;
-  if (value == k) return true;
-  if (value == 3) return true;
-  mpz_class s, d, b, e, x;
+#include <iostream>
 
-  for (s = 0, d = value - 1; !(d && 1); s++) {
-    d >>= 1;
-  }
+#include <limits>
+#include <vector>
+#include <string>
+#include <utility>  // std::pair
+#include <fstream>
 
-  for (x = 1, b = k % value, e = d; e; e >>= 1) {
-    if (e && 1) {
-      x = (x * b) % value;
-    }
-  }
+#include <cstdlib>
+#include <cassert>
 
-  if (x == 1 || x == value - 1) {
-    return true;
-  }
+#include <dgcrypto/dgcrypto.hh>
 
-  while (s-- > 1) {
-    x = (x * x) % value;
-    if (x == 1) {
-      return false;
-    }
-    if (x == value - 1) {
-      return true;
-    }
-  }
-  return false;
-}
+// iostream
+using std::ios;
+using std::ios_base;
+using std::cout;
+using std::cerr;
+using std::to_string;
+using std::streamsize;
+// vector
+using std::vector;
+// string
+using std::string;
+// utility
+using std::pair;
+// cstdlib
+using std::exit;
+// fstream
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
+// limits
+using std::numeric_limits;
 
 // returns a vector of pairs that correspond to the found factors
 vector<pair<mpz_class, mpz_class>> findPrimeFactors(mpz_class n) {
   vector<pair<mpz_class, mpz_class>> factors;
+  try {
+    // string name_of_file = findOrGeneratePrimes();
+    ifstream prime_file;
+    prime_file.open("prime_list_1000000", ios::in);
+    // trial division method...
+    // ignore 0 and 1, since they're not valid factors
 
-  // string name_of_file = findOrGeneratePrimes();
-  fstream prime_file;
-  prime_file.open("lib/rsa-attack-lib/prime_list_1000000", ios::in);
-  // trial division method...
-  // ignore 0 and 1, since they're not valid factors
-  mpz_class i = 0;
-  mpz_class prime_candidate = i;
-  while ((prime_candidate * prime_candidate) <= n) {
-    prime_candidate = getNextPrime(prime_file, i);
-    if ((n % prime_candidate) == 0) {
-      mpz_class first_factor = prime_candidate;
-      mpz_class other_factor = (n / prime_candidate);
-      if (is_prime(other_factor) && is_prime(first_factor)) {
-        factors.push_back(
-            pair<mpz_class, mpz_class>(first_factor, other_factor));
-        return factors;
+    // while ((prime_candidate * prime_candidate) <= n) {
+    // sam: use a for loop for indicies
+    for (mpz_class i = 0, prime_candidate = 0;
+         (prime_candidate * prime_candidate) <= n; ++i) {
+      prime_candidate = getNextPrime(prime_file, i);
+      if ((n % prime_candidate) == 0) {
+        mpz_class first_factor = prime_candidate;
+        mpz_class other_factor = (n / prime_candidate);
+        if (dgrprime::is_prime(other_factor) &&
+            dgrprime::is_prime(first_factor)) {
+          factors.push_back(
+              pair<mpz_class, mpz_class>(first_factor, other_factor));
+          return factors;
+        }
       }
+      // sam: use a for loop for indicies
+      // ++i;
     }
-    ++i;
+  } catch (ios_base::failure& e) {
+    cerr << "Caught ios_base::failure: " << __PRETTY_FUNCTION__ << "\n";
+    cerr << "Exception: " << e.what() << "\n";
+    cerr << "Error Code: " << e.code() << "\n";
   }
-
   return factors;
 }
 
 mpz_class getNthPrime(mpz_class n) {
-  fstream prime_file;
-  prime_file.open("lib/rsa-attack-lib/prime_list_1000000", ios::in);
-  skipToLine(prime_file, n);
-  getNextPrime(prime_file, n);
+  // sam: find a way to get the file open, then move all operations inside the
+  //      try-catch.
+  //      catch only catches inside the try block.
+  try {
+    ifstream prime_file;
+    prime_file.exceptions(ifstream::failbit | ifstream::badbit);
+    prime_file.open("prime_list_1000000", ios::in);
+    skipToLine(prime_file, n);
+    getNextPrime(prime_file, n);
+  } catch (ios_base::failure& e) {
+    // } catch (ifstream::failure e) {
+    // cerr << "Exception opening/reading/closing file\n";
+
+    // sam: changed exception to a reference and you should cerr e.what() also
+    // sam: also this is where you throw in every test
+    cerr << "Caught ios_base::failure: " << __PRETTY_FUNCTION__ << "\n";
+    cerr << "Exception: " << e.what() << "\n";
+    cerr << "Error Code: " << e.code() << "\n";
+    // sam: are you really sure you want to abort? I always thought catching
+    // exceptions was in order to not just abort
+    // exit(EXIT_FAILURE);
+  }
+  // you never return here!
+  // do you want return getNextPrime?
 }
 
-mpz_class getNextPrime(fstream& prime_file, mpz_class n) {
+// sam: what is mpz_class n there for? You get a warning that it's unused
+// also you return a num but never use it
+mpz_class getNextPrime(ifstream& prime_file, mpz_class n) {
   mpz_class num = -1;
-  prime_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  // sam: this goes in the try block
+  // prime_file.exceptions(ifstream::failbit | ifstream::badbit);
   try {
+    prime_file.exceptions(ifstream::failbit | ifstream::badbit);
     prime_file >> num;
-  } catch (std::ifstream::failure e) {
-    std::cerr << "Exception opening/reading/closing file\n";
+  } catch (ios_base::failure& e) {
+    // } catch (ifstream::failure e) {
+    // cerr << "Exception opening/reading/closing file\n";
+
+    // sam: changed exception to a reference and you should cerr e.what() also
+    // sam: also this is where you throw in every test
+    // cerr << "Exception opening/reading/closing file: " << e.what() <<
+    // "\n";
+    cerr << "Caught ios_base::failure: " << __PRETTY_FUNCTION__ << "\n";
+    cerr << "Exception: " << e.what() << "\n";
+    cerr << "Error Code: " << e.code() << "\n";
     exit(EXIT_FAILURE);
   }
   return num;
 }
 
+// sam: if this is the case -- maybe we should have it in a seperate binary?
+//      I'll do it if you say the word.
 // Note to TA, we don't actually use this in our tools, we only use it to
 // generate a PrimeList file for later use
 void generateNPrimes(long int n) {
-  ofstream prime_list_file;
-  prime_list_file.open("prime_list_" + to_string(n));
-  mpz_class prime;
+  try {
+    ofstream prime_list_file;
+    prime_list_file.open("prime_list_" + to_string(n));
+    mpz_class prime;
 
-  for (int i = 0; i < n; ++i) {
-    mpz_nextprime(prime.get_mpz_t(), prime.get_mpz_t());
-    prime_list_file << prime << "\n";
+    // sam: n is a long int, i should be too
+    for (long int i = 0; i < n; ++i) {
+      mpz_nextprime(prime.get_mpz_t(), prime.get_mpz_t());
+      prime_list_file << prime << "\n";
+    }
+    prime_list_file.close();
+  } catch (ios_base::failure& e) {
+    cerr << "Caught ios_base::failure: " << __PRETTY_FUNCTION__ << "\n";
+    cerr << "Exception: " << e.what() << "\n";
+    cerr << "Error Code: " << e.code() << "\n";
   }
-  prime_list_file.close();
 }
 
-fstream& skipToLine(fstream& file, mpz_class num) {
-  for (int i = 0; i < num; ++i) {
-    file.ignore(numeric_limits<streamsize>::max(), '\n');
+ifstream& skipToLine(ifstream& file, mpz_class num) {
+  try {
+    // sam: num is an mpz_class, i should be too
+    for (mpz_class i = 0; i < num; ++i) {
+      file.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+  } catch (ios_base::failure& e) {
+    cerr << "Caught ios_base::failure: " << __PRETTY_FUNCTION__ << "\n";
+    cerr << "Exception: " << e.what() << "\n";
+    cerr << "Error Code: " << e.code() << "\n";
   }
   return file;
 }
--- lib/rsa-attack-lib/rsa-attack-lib.hh
+++ lib/rsa-attack-lib/rsa-attack-lib.hh
@@ -1,14 +1,33 @@
 #pragma once
 
-#include <gmpxx.h>
 #include <vector>
 #include <utility>
 #include <fstream>
 
+#include <gmpxx.h>
+
+// sam: this stuff should really be a class -- otherwise you have to understand
+//      your implementation to use these functions without doing something you
+//      do not want.
+//      notably, you open a file and close it in different functions
+//      maybe in the class version open in the constructor and close in the
+//      destructor? (not totally sure that is good advice)
+//      I've also left some comments in the cc
+//      I also moved you to using std::blah, and removed all std:: from the file
+//
+// TODO:
+//      - [ ] put in class (or atleast a namespace, these shouldn't be global)
+//      - [ ] fix exceptions
+//      - [ ] dedicate to either writing std or using std::blah, a mix is hard to
+//            read
 std::vector<std::pair<mpz_class, mpz_class>> findPrimeFactors(mpz_class n);
+
 void generateNPrimes(long int n);
-std::fstream& skipToLine(std::fstream& file, mpz_class num);
+
+std::ifstream& skipToLine(std::ifstream& file, mpz_class num);
+
 mpz_class getNthPrime(mpz_class n);
+
 mpz_class getNextPrime(
-    std::fstream& prime_file,
+    std::ifstream& prime_file,
     mpz_class n);  // this function is faster since you can hold onto your place

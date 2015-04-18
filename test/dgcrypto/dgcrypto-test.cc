ad8d3c3ebeedabdeba29a1a381528e4aaa686d14
Sam Gwydir <sam@samgwydir.com>
2015-04-18 07:58:20 -0500

    now compiles
    - I think a merge error caused it, but there were nested tests and that
      is not allowed by gtest

--- test/dgcrypto/dgcrypto-test.cc
+++ test/dgcrypto/dgcrypto-test.cc
@@ -9,7 +9,6 @@ TEST(DGCryptoPrime, Prime2b) {
   dgrprime two(2);
 
   EXPECT_EQ(2u, two.bits());
-  
 }
 
 TEST(DGCryptoPrime, Prime4b) {
@@ -36,25 +35,10 @@ TEST(DGCryptoPrime, Prime16b) {
   EXPECT_GT(65535, dgrprime::is_prime(sixteen.value()));
 }
 
-TEST(DGCryptoPrime, isPrime2) {
-  // create a prime with 2 bits
-  dgrprime tree(3);
-
-  EXPECT_EQ(3, tree.value());
-
-TEST(DGCryptoPrime, isPrime3) {
-  EXPECT_TRUE(dgrprime::is_prime(3_mpz));
-}
-
-TEST(DGCryptoPrime, isPrime11) {
-  EXPECT_TRUE(dgrprime::is_prime(11_mpz));
-}
-
-TEST(DGCryptoPrime, isPrime59) {
-  EXPECT_TRUE(dgrprime::is_prime(59_mpz));
-}
-
+TEST(DGCryptoPrime, isPrime2) { EXPECT_TRUE(dgrprime::is_prime(2_mpz)); }
+TEST(DGCryptoPrime, isPrime3) { EXPECT_TRUE(dgrprime::is_prime(3_mpz)); }
+TEST(DGCryptoPrime, isPrime11) { EXPECT_TRUE(dgrprime::is_prime(11_mpz)); }
+TEST(DGCryptoPrime, isPrime59) { EXPECT_TRUE(dgrprime::is_prime(59_mpz)); }
 TEST(DGCryptoPrime, isPrime15484517) {
   EXPECT_TRUE(dgrprime::is_prime(15484517_mpz));
 }
-

64b94af0da3c3a7b50b526b6f98dfcea5529e498
Sam Gwydir <sam@samgwydir.com>
2015-04-18 07:58:12 -0500

    remove commented code

--- bin/coverage_list.cmake
+++ bin/coverage_list.cmake
@@ -6,15 +6,7 @@ if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     list(APPEND COVERAGE_SRCS ${BUILD_LIST})
 
     include(${PROJECT_SOURCE_DIR}/lib/coverage_list.cmake)
-    include(${PROJECT_SOURCE_DIR}/test/coverage_list.cmake)
-    # file(STRINGS ${PROJECT_SOURCE_DIR}/lib/coverage_list.cmake LIB_COVERAGE_LIST)
-    # ## print out list for debugging
-    # #message(${LIB_COVERAGE_LIST})
-    # list(APPEND COVERAGE_SRCS ${LIB_COVERAGE_LIST})
-    # file(STRINGS ${PROJECT_SOURCE_DIR}/test/coverage_list.cmake TEST_COVERAGE_LIST)
-    # ## print out list for debugging
-    # #message(${TEST_COVERAGE_LIST})
-    # list(APPEND COVERAGE_SRCS ${TEST_COVERAGE_LIST})
+    # include(${PROJECT_SOURCE_DIR}/test/coverage_list.cmake)
 
     # Create the coveralls target.
     coveralls_setup(

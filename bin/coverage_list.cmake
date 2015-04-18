6ba9b0226cf9b0e81629149e267701c332a2eac4
Sam Gwydir <sam@samgwydir.com>
2015-04-18 06:58:03 -0500

    auto file listing works and openSSL is linked

--- bin/coverage_list.cmake
+++ bin/coverage_list.cmake
@@ -1,8 +1,20 @@
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
   if (COVERALLS)
-    list(APPEND COVERAGE_SRCS include(build_list.cmake))
+    file(STRINGS build_list.cmake BUILD_LIST)
+    ## print out list for debugging
+    #message(${BUILD_LIST})
+    list(APPEND COVERAGE_SRCS ${BUILD_LIST})
+
     include(${PROJECT_SOURCE_DIR}/lib/coverage_list.cmake)
-    # include(${PROJECT_SOURCE_DIR}/test/coverage_list.cmake)
+    include(${PROJECT_SOURCE_DIR}/test/coverage_list.cmake)
+    # file(STRINGS ${PROJECT_SOURCE_DIR}/lib/coverage_list.cmake LIB_COVERAGE_LIST)
+    # ## print out list for debugging
+    # #message(${LIB_COVERAGE_LIST})
+    # list(APPEND COVERAGE_SRCS ${LIB_COVERAGE_LIST})
+    # file(STRINGS ${PROJECT_SOURCE_DIR}/test/coverage_list.cmake TEST_COVERAGE_LIST)
+    # ## print out list for debugging
+    # #message(${TEST_COVERAGE_LIST})
+    # list(APPEND COVERAGE_SRCS ${TEST_COVERAGE_LIST})
 
     # Create the coveralls target.
     coveralls_setup(
--- test/coverage_list.cmake
+++ test/coverage_list.cmake
@@ -1,5 +1,8 @@
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
   if (COVERALLS)
-    list(APPEND COVERAGE_SRCS include(build_list.cmake))
+    file(STRINGS ${PROJECT_SOURCE_DIR}/test/build_list.cmake BUILD_LIST)
+    ## print out list for debugging
+    #message(${BUILD_LIST})
+    list(APPEND COVERAGE_SRCS ${BUILD_LIST})
   endif()
 endif()

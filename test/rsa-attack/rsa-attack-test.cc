939f81a1135affc49eb18d37408727513aca6378
Christopher Findeisen <cfindeisen7@gmail.com>
2015-04-22 11:32:56 -0500

    Fix compilation errors

--- test/rsa-attack/rsa-attack-test.cc
+++ test/rsa-attack/rsa-attack-test.cc
@@ -45,12 +45,12 @@ TEST(factorize, largerN) {
 }
 
 //test based from example in http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-TEST(ExtendedEuclidean, positiveNumbers){
+TEST(ExtendedEuclidean, wikipediaExample){
   mpz_class a = 240;
   mpz_class b = 46;
   mpz_class expected_gcd = 2;
 
-  mpz_class actual_gcd = extended_euclidean(a,b);
+  mpz_class actual_gcd = rsatk::extended_euclidean(a,b);
   EXPECT_EQ(expected_gcd, actual_gcd);
 }
 
@@ -62,38 +62,45 @@ protected:
   }
   //tear down?
 
-  static const int p = 11; //we get this from factorization
-  static const int q = 13;
-  static const int e = 7; //given(supposedly) in public key
-  static const string m = "attack at dawn"; //for testing
-  int n;
+  mpz_class p = 11; //we get this from factorization
+  mpz_class q = 13;
+  mpz_class e = 7; //given(supposedly) in public key
+  string m = "attack at dawn"; //for testing
+  mpz_class n;
 };
 
 
-TEST_F(CrackingRSATest, calculateTotient){
-  int expected_totient = 120;
-  int actual_totient = rsatk::calculate_totient(p, q);
-  EXPECT_EQ(expected_totient, actual_totient);
-}
+//namespace crt = CrackingRSATest;
+  TEST_F(CrackingRSATest, calculateTotient){
+    mpz_class expected_totient = 120;
+    mpz_class actual_totient = rsatk::calculate_totient(p, q);
+    EXPECT_EQ(expected_totient, actual_totient);
+  }
 
-//TEST(CrackingRSATest, findE){
-//EXPECT_EQ(0,1);
-//}
-TEST(CrackingRSATest, caclculateD){
-  int expected_d = 103;
-  int totient = rsatk::calculate_totient(p, q);
-  ASSERT_EQ(120, totient);
-  int actual_d = rsatk::calculate_d(totient, e);
-  EXPECT_EQ(expected_d, actual_d);
-}
+  //TEST(CrackingRSATest, findE){
+  //EXPECT_EQ(0,1);
+  //}
+  TEST_F(CrackingRSATest, calculateD){
+    mpz_class expected_d = 103;
+    mpz_class totient = rsatk::calculate_totient(p, q);
+    ASSERT_EQ(120, totient);
+    mpz_class actual_d = rsatk::calculate_d(totient, e);
+    EXPECT_EQ(expected_d, actual_d);
+  }
 
-TEST(CrackingRSATest, applyPrivateKey){
-  std::string encrypted_message = "35052111338673026690212423937053328511880760811579981620642802346685810623109850235943049080973386241113784040794704193978215378499765413083646438784740952306932534945195080183861574225226218879827232453912820596886440377536082465681750074417459151485407445862511023472235560823053497791518928820272257787786";
+  TEST_F(CrackingRSATest, applyPrivateKey){
+    string encrypted_message = "35052111338673026690212423937053328511880760811579981620642802346685810623109850235943049080973386241113784040794704193978215378499765413083646438784740952306932534945195080183861574225226218879827232453912820596886440377536082465681750074417459151485407445862511023472235560823053497791518928820272257787786";
 
-  decrypted_message = decrypt_message(encrypted_message, n, d);
+    mpz_class totient = rsatk::calculate_totient(p, q);
+    ASSERT_EQ(120, totient);
+    mpz_class d = rsatk::calculate_d(totient, e);
+    ASSERT_EQ(103, d);
 
-  EXPECT_EQ(m, decrypted_message);
-}
+    string decrypted_message = rsatk::decrypt_message(encrypted_message, n, d);
+
+    EXPECT_EQ(m, decrypted_message);
+  }
+
 
 
 // TEST(Primes, Generate){

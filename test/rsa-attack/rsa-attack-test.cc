6805f4102ac513999252f6cebff290516dc4b901
Christopher Findeisen <cfindeisen7@gmail.com>
2015-04-22 11:13:21 -0500

    Add CrackingRSA text fixture and necessary tests

--- test/rsa-attack/rsa-attack-test.cc
+++ test/rsa-attack/rsa-attack-test.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <gmpxx.h>
 #include <vector>
+#include <string>
 #include <rsa-attack-lib/rsa-attack-lib.hh>
 using namespace std;
 
@@ -43,23 +44,55 @@ TEST(factorize, largerN) {
   EXPECT_EQ(expected_outputs, rsatk::findPrimeFactors(big_n));
 }
 
-TEST(itermediate, generateTotient){
-   EXPECT_EQ(0,1);
- }
-TEST(itermediate, findE){
-  EXPECT_EQ(0,1);
+//test based from example in http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+TEST(ExtendedEuclidean, positiveNumbers){
+  mpz_class a = 240;
+  mpz_class b = 46;
+  mpz_class expected_gcd = 2;
+
+  mpz_class actual_gcd = extended_euclidean(a,b);
+  EXPECT_EQ(expected_gcd, actual_gcd);
 }
-TEST(decrypt, generateDSimple){
-  EXPECT_EQ(0,1);
+
+//test pulled from here: http://doctrina.org/How-RSA-Works-With-Examples.html
+class CrackingRSATest : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    n = p * q;
+  }
+  //tear down?
+
+  static const int p = 11; //we get this from factorization
+  static const int q = 13;
+  static const int e = 7; //given(supposedly) in public key
+  static const string m = "attack at dawn"; //for testing
+  int n;
+};
+
+
+TEST_F(CrackingRSATest, calculateTotient){
+  int expected_totient = 120;
+  int actual_totient = rsatk::calculate_totient(p, q);
+  EXPECT_EQ(expected_totient, actual_totient);
 }
-TEST(decrypt, generateDComplex){
-  EXPECT_EQ(0,1);
+
+//TEST(CrackingRSATest, findE){
+//EXPECT_EQ(0,1);
+//}
+TEST(CrackingRSATest, caclculateD){
+  int expected_d = 103;
+  int totient = rsatk::calculate_totient(p, q);
+  ASSERT_EQ(120, totient);
+  int actual_d = rsatk::calculate_d(totient, e);
+  EXPECT_EQ(expected_d, actual_d);
 }
-TEST(decrypt, generatePrivateKey){
-  EXPECT_EQ(0,1);
-}
-TEST(decrypt, applyPrivateKey){
-  EXPECT_EQ(0,1);
+
+TEST(CrackingRSATest, applyPrivateKey){
+  std::string encrypted_message = "35052111338673026690212423937053328511880760811579981620642802346685810623109850235943049080973386241113784040794704193978215378499765413083646438784740952306932534945195080183861574225226218879827232453912820596886440377536082465681750074417459151485407445862511023472235560823053497791518928820272257787786";
+
+  decrypted_message = decrypt_message(encrypted_message, n, d);
+
+  EXPECT_EQ(m, decrypted_message);
 }
 
 

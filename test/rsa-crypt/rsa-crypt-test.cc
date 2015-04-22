8a644c6354f11b34c1688df6b3b509cc8f4eadd4
Rafa Moreno <rmoreno.cesar@gmail.com>
2015-04-22 18:05:17 -0500

    wrote test for e, n, and phi_n

--- test/rsa-crypt/rsa-crypt-test.cc
+++ test/rsa-crypt/rsa-crypt-test.cc
@@ -1,7 +1,10 @@
 #include <gtest/gtest.h>
 #include <rsa-crypt-lib/rsa-crypt-lib.hh>
+#include <cstdlib.h>
 TEST(CryptStub, oneEqualsone) {
   int one = 1;
+  RsaKeys::RsaKeys publickey = new RsaKeys(512);
+  
   EXPECT_EQ(1, one);
 }
 

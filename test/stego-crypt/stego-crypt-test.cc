08d105286817c2e49f731db10f889e6c115be4bb
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-23 06:01:55 -0500

    Changed interface a little bit, and implemented embedding

--- test/stego-crypt/stego-crypt-test.cc
+++ test/stego-crypt/stego-crypt-test.cc
@@ -1,25 +1,13 @@
 #include <gtest/gtest.h>
 #include <stego-crypt-lib/stego-crypt-lib.hh>
 
-TEST(Steg, LSB1) {
+TEST(StegoCryptBMP, LSB) {
   std::string test_string = "The quick brown fox jumped over the lazy dog.";
-  Embedding_agent embedder("test/stego-crypt/test.bmp",
-                           "test/stego-crypt/test-stego.bmp");
+  Embedding_agent embedder("../../test/stego-crypt/test.bmp",
+                           "../../test/stego-crypt/test-stego.bmp");
   embedder.embed_and_save(test_string);
 
-  Extracting_agent extractor("test/stego-crypt/test-stego.bmp");
-  std::string extracted_string = extractor.extract();
-
-  EXPECT_EQ(test_string, extracted_string);
-}
-
-TEST(Steg, LSB2) {
-  std::string test_string = "The quick brown fox jumped over the lazy dog.";
-  Embedding_agent embedder("test/stego-crypt/test.bmp",
-                           "test/stego-crypt/test-stego.bmp", 2);
-  embedder.embed_and_save(test_string);
-
-  Extracting_agent extractor("test/stego-crypt/test-stego.bmp", 2);
+  Extracting_agent extractor("../../test/stego-crypt/test-stego.bmp");
   std::string extracted_string = extractor.extract();
 
   EXPECT_EQ(test_string, extracted_string);

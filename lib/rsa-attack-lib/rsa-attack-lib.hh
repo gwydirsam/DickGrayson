493918ee1554fa009501a814fec38e1240cf4155
Christopher Findeisen <cfindeisen7@gmail.com>
2015-04-24 14:07:49 -0500

    find_prime.. returns a pair now.added a parse fctn

--- lib/rsa-attack-lib/rsa-attack-lib.hh
+++ lib/rsa-attack-lib/rsa-attack-lib.hh
@@ -14,21 +14,31 @@
 // m is the plain_test
 namespace rsatk {
 
-static const std::string relative_path = "../../lib/rsa-attack-lib/";
+  struct RSA_data {
+    mpz_class C = -1;        // cipher text
+    mpz_class n = -1;        // p*q
+    mpz_class e = -1;        // exponent of pub key
+    mpz_class p = -1;        // prime factor
+    mpz_class q = -1;        // ''
+    mpz_class totient = -1;  // (p - 1) * (q - 1)
+  };
 
-std::vector<std::pair<mpz_class, mpz_class>> find_prime_factors(mpz_class n);
-void generate_n_primes(mpz_class n);
+  static const std::string relative_path = "../../lib/rsa-attack-lib/";
 
-// this function uses pre-cached prime_lists
-mpz_class get_next_prime(std::ifstream& prime_file);
+  std::pair<mpz_class, mpz_class> find_prime_factors(mpz_class n);
+  void generate_n_primes(mpz_class n);
 
-mpz_class calculate_totient(mpz_class p, mpz_class q);
+  // this function uses pre-cached prime_lists
+  mpz_class get_next_prime(std::ifstream& prime_file);
 
-mpz_class calculate_d(mpz_class totient, mpz_class e);
+  mpz_class calculate_totient(mpz_class p, mpz_class q);
 
-// extended_euclidean(a, b) returns three values:
-// x, y, gcd, such that ax + by = gcd(a, b)
-std::vector<mpz_class> extended_euclidean(mpz_class totient, mpz_class e);
-std::string decrypt_message(std::string encrypted, mpz_class n, mpz_class d);
+  mpz_class calculate_d(mpz_class totient, mpz_class e);
 
+  // extended_euclidean(a, b) returns three values:
+  // x, y, gcd, such that ax + by = gcd(a, b)
+  std::vector<mpz_class> extended_euclidean(mpz_class totient, mpz_class e);
+  std::string decrypt_message(std::string encrypted, mpz_class n, mpz_class d);
+
+  RSA_data parse_rsa_file(std::fstream& fname);
 }

e4701c151e7f3504a8fea68af6caee99f018688b
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-26 19:44:41 -0500

    Removed unnecessary exception definitions [ci skip]

--- lib/dgsnd/wav.hh
+++ lib/dgsnd/wav.hh
@@ -7,16 +7,6 @@ namespace Sound {
 class WAV {
 public:
   struct Invalid_format_exception : public std::exception {
-    virtual const char* what() const throw() {
-      return "WAV: Invalid_format_exception";
-    }
-  } invalid_format;
-
-  struct Inaccessible_file_exception : public std::exception {
-    virtual const char* what() const throw() {
-      return "WAV: Inaccessible_file_exception";
-    }
-  } inaccessible_file;
 
   WAV(const std::string& fname) { open(fname); }
 

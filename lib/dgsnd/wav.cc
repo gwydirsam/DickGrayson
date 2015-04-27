4c4a2bf00ab7f4489afc34435838a48f1244b880
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-26 19:43:10 -0500

    Fleshing out WAV interface

---
+++ lib/dgsnd/wav.cc
@@ -0,0 +1,15 @@
+#include "wav.hh"
+
+namespace DG {
+namespace Sound {
+
+void WAV::open(const std::string& fname) {
+  std::move(fname); // do fun things until we define this function
+}
+
+void WAV::write(const std::string& fname) {
+  std::move(fname); // do fun things until we define this functio
+}
+
+}
+}
--- lib/dgsnd/wav.hh
+++ lib/dgsnd/wav.hh
@@ -1,11 +1,29 @@
 #include <sndfile.h>
+#include <string>
 
 namespace DG {
 namespace Sound {
 
 class WAV {
 public:
-  WAV() { }
+  struct Invalid_format_exception : public std::exception {
+    virtual const char* what() const throw() {
+      return "WAV: Invalid_format_exception";
+    }
+  } invalid_format;
+
+  struct Inaccessible_file_exception : public std::exception {
+    virtual const char* what() const throw() {
+      return "WAV: Inaccessible_file_exception";
+    }
+  } inaccessible_file;
+
+  WAV(const std::string& fname) { open(fname); }
+
+  void open(const std::string& fname);
+
+  // write wav out to the file fname
+  void write(const std::string& fname);
 private:
 };
 

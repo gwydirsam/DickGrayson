9a0738de7ded4decfc413cda7eb5fff920aba893
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-28 18:37:56 -0500

    WAV embedding agent stub class and functions added

---
+++ lib/stego-crypt-lib/wav_embedding_agent.cc
@@ -0,0 +1,24 @@
+#include "wav_embedding_agent.hh"
+
+void WAV_embedding_agent::embed() {
+  // TODO: implement
+}
+
+void WAV_embedding_agent::save() const {
+  // TODO: implement
+}
+
+std::vector<unsigned> WAV_embedding_agent::message_to_masks(const std::string& msg, Mask_type mask_type) {
+  // TODO: implement
+  // do fun things until we implement function for real
+  std::move(msg);
+  std::move(mask_type);
+  return std::vector<unsigned>();
+}
+
+bool WAV_embedding_agent::check_msg_media_capacity(const std::string& msg) {
+  // TODO: implement
+  // do fun things until we implement function for real
+  std::move(msg);
+  return false;
+}

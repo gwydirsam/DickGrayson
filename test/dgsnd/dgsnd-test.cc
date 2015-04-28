3d88e52680a0544d6c431a0b7892267ace912eac
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-27 20:46:52 -0500

    Fixed logic error in tests, corrected test masks

--- test/dgsnd/dgsnd-test.cc
+++ test/dgsnd/dgsnd-test.cc
@@ -15,20 +15,20 @@ TEST(Wav, SampleSetMask) {
   dgwav wav("../../test/dgsnd/test.wav");
   unsigned first_sample = wav.get_sample(0);
 
-  wav.sample_set_mask(0, 0xFFFFu);
-  EXPECT_EQ(0xFFFFu, wav.get_sample(0));
-
   wav.sample_set_mask(0, 0x0u);
   EXPECT_EQ(first_sample, wav.get_sample(0));
+
+  wav.sample_set_mask(0, 0xFFFFFFFFu);
+  EXPECT_EQ(0xFFFFFFFFu, wav.get_sample(0));
 }
 
 TEST(Wav, SampleUnsetMask) {
   dgwav wav("../../test/dgsnd/test.wav");
   unsigned first_sample = wav.get_sample(0);
 
-  wav.sample_set_mask(0, 0xFFFFu);
-  EXPECT_EQ(0x0u, wav.get_sample(0));
-
-  wav.sample_set_mask(0, 0x0u);
+  wav.sample_unset_mask(0, 0x0u);
   EXPECT_EQ(first_sample, wav.get_sample(0));
+
+  wav.sample_unset_mask(0, 0xFFFFFFFFu);
+  EXPECT_EQ(0x0u, wav.get_sample(0));
 }

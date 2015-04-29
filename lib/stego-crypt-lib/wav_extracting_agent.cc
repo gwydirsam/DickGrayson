5bcc3d70124d614fbc9682105f737935971d8ea4
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-28 20:38:53 -0500

    Added wav extracting agent stubs

---
+++ lib/stego-crypt-lib/wav_extracting_agent.cc
@@ -0,0 +1,13 @@
+#include "wav_extracting_agent.hh"
+
+std::string WAV_extracting_agent::extract() const {
+  // TODO: implement
+  return "";
+}
+
+std::vector<bool> WAV_extracting_agent::extract_bit_from_sample(int index) const {
+  // TODO: implement
+  // do fun things until we implement the function for real
+  std::move(index);
+  return std::vector<bool>();
+}
---
+++ lib/stego-crypt-lib/wav_extracting_agent.hh
@@ -0,0 +1,16 @@
+#include <dgsnd/dgsnd.hh>
+#include "abstract_extracting_agent.hh"
+
+class WAV_extracting_agent : public Abstract_extracting_agent {
+public:
+  WAV_extracting_agent(std::string inwav_fname) {} //: inwav(inwav_fname) {}
+
+  virtual std::string extract() const override;
+
+private:
+  //dgwav inwav;
+
+  //// private helper functions
+
+  std::vector<bool> extract_bit_from_sample(int index) const;
+};

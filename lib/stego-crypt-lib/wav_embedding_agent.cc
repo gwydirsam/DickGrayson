b1405cf95a6e7dc2e357f0a04fc2cb2afa59ed17
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-29 01:17:07 -0500

    Completed implementation of WAV steg, tests now pass

--- lib/stego-crypt-lib/wav_embedding_agent.cc
+++ lib/stego-crypt-lib/wav_embedding_agent.cc
@@ -1,24 +1,43 @@
 #include "wav_embedding_agent.hh"
 
 void WAV_embedding_agent::embed() {
-  // TODO: implement
+  int num_samples = outwav.get_num_samples();
+
+  for (int i = 0; i < num_samples; ++i) {
+    outwav.sample_unset_mask(i, 0x1);
+    outwav.sample_set_mask(i,  set_masks[i]);
+    unsigned next_index = i + 1;
+    if (next_index >= set_masks.size()) {
+      break;
+    }
+  }
 }
 
 void WAV_embedding_agent::save() const {
-  // TODO: implement
+  outwav.write(outwav_fname);
 }
 
 std::vector<unsigned> WAV_embedding_agent::message_to_masks(const std::string& msg, Mask_type mask_type) {
-  // TODO: implement
-  // do fun things until we implement function for real
-  std::move(msg);
-  std::move(mask_type);
-  return std::vector<unsigned>();
+  std::vector<unsigned> masks;
+  std::vector<bool> msg_bits = message_to_bits(msg);
+
+  for (int i = 0; (unsigned)i < msg_bits.size(); ++i) {
+    unsigned mask = 0;
+    switch (mask_type) {
+    case Mask_type::SET:
+      mask |= msg_bits[i];
+      break;
+    case Mask_type::UNSET:
+      mask |= !msg_bits[i];
+      break;
+    }
+    masks.push_back(mask);
+  }
+  return masks;
 }
 
 bool WAV_embedding_agent::check_msg_media_capacity(const std::string& msg) {
-  // TODO: implement
-  // do fun things until we implement function for real
-  std::move(msg);
-  return true;
+  unsigned num_bits = msg.length() * 8;
+  unsigned num_samples = inwav.get_num_samples();
+  return num_bits < num_samples;
 }
--- lib/stego-crypt-lib/wav_extracting_agent.hh
+++ lib/stego-crypt-lib/wav_extracting_agent.hh
@@ -3,14 +3,15 @@
 
 class WAV_extracting_agent : public Abstract_extracting_agent {
 public:
-  WAV_extracting_agent(std::string inwav_fname) {} //: inwav(inwav_fname) {}
+  WAV_extracting_agent(std::string inwav_fname) : inwav(inwav_fname) {}
 
   virtual std::string extract() const override;
 
 private:
-  //dgwav inwav;
+  dgwav inwav;
 
   //// private helper functions
 
-  std::vector<bool> extract_bit_from_sample(int index) const;
+  // returns true when the lsb is 1, false otherwise
+  bool extract_bit_from_sample(int index) const;
 };

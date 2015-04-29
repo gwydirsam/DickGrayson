4790c8985bfdfa7777c473c5792617bcdae25600
Christopher Findeisen <cfindeisen7@gmail.com>
2015-04-29 17:18:54 -0500

    Adding stubs for testing low exp

---
+++ lib/rsa-attack-lib/low_exponent.cc
@@ -0,0 +1,9 @@
+#include "low_exponent.hh"
+
+namespace lowexp {
+
+using namespace std;
+  mpz_class low_exponent_attack(rsatk::RSA_data m_1, rsatk::RSA_data m_2, rsatk::RSA_data m_3){
+    return -1;//function stub for testing
+  }
+}

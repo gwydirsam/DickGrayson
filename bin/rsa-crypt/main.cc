de1470db2abb6788793b7ee90f2b396ae8385f83
Rafa Moreno <rmoreno.cesar@gmail.com>
2015-05-11 10:30:08 -0500

    pushing for sam <3

--- bin/rsa-crypt/main.cc
+++ bin/rsa-crypt/main.cc
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[]) {
   std::cout << "CSCE315 Project 4" << std::endl;
-  int bits;
+ /* int bits;
   std::string message;
 
   std::cout<<"What size bit numbers do you want to generate the RSA Key with? (up to 1024)"<<std::endl;
@@ -27,25 +27,46 @@ int main(int argc, char *argv[]) {
 
   std::string decrypted_message = r1.decrypt_message(encrypted_message);
   std::cout<<"Decrypted Message : "<<decrypted_message<<std::endl;
+ */
+  RsaKeys r1(512);
+  int opt;
+  std::ofstream of1; 
+  std::ofstream of2;
 
-  
+  std::ifstream if1;
+  std::ifstream if2;
 
- /* int opt;
-  ostream f1; 
-  while((opt = getopt(argc, argv, "n:t:")) != -1) {
+  std::string message_file_path;
+  std::string message;
+  std::string encrypted_message;
+  std::string decrypted_message;
+
+  while((opt = getopt(argc, argv, "i:o:m:")) != -1) {
     switch(opt) {
-      case 'n' : 
-        std::string fname = std::string(optarg);
-        f1.open(fname);
-        if(!f1.is_open()){
-          std::cout<<fname << " : error"<<std::endl;
+      case 'i' : 
+        message_file_path = std::string(optarg);
+        of1.open(message_file_path);
+        if(!of1.is_open()){
+          std::cout<<message_file_path << " : error"<<std::endl;
         }
         break;
       
-      case 't' :
-        
-    }
-
-   
-  }*/
+      case 'o' :
+        std::string output_file_path = std::string(optarg);
+        of2.open(output_file_path); 
+        if(!of2.is_open()){
+          std::cout<<output_file_path<< " : error"<<std::endl;
+        } 
+        break;
+    } 
+  }  
+  
+  if1.open(message_file_path);
+  getline(if1, message);
+  encrypted_message =  r1.encrypt_message(message); 
+  if1.close();
+  
+  if(of2.is_open()){
+    of2<<r1.decrypt_message(encrypted_message);
+  } 
 }

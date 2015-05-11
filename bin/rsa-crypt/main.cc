4a88f6f7218f3fe347b00b3a7e77eb4883a3b638
Rafa Moreno <rmoreno.cesar@gmail.com>
2015-05-10 23:43:16 -0500

    made simple interface for backup just in case getopt doesn't go well. started on getopt

--- bin/rsa-crypt/main.cc
+++ bin/rsa-crypt/main.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <getopt.h>
+#include <fstream>
 #include "rsa-crypt-lib/rsa-crypt-lib.hh"
 
 #include <gmpxx.h>
@@ -6,10 +8,41 @@
 int main(int argc, char *argv[]) {
   std::cout << "CSCE315 Project 4" << std::endl;
   int bits;
-  std::cout<<"What size bit numbers do you want to generate?"<<std::endl;
+  std::string message;
+
+  std::cout<<"What size bit numbers do you want to generate the RSA Key with? (up to 1024)"<<std::endl;
   std::cin>>bits;
- 
-  RsaKeys my_keys(bits);
+
+  if(!isDigit(bits)){
+    cout<<"Did not enter a valid number"
+    return 0;
+  } 
   
-  return 0;
-}
+  std::cout<<"What message do you want to encode?"<<std::endl;
+  std::cin>>message;
+
+  RsaKeys r1(bits);
+  std::string encrypted_message = r1.encrypt_message(message); 
+  std::cout<<"Encrypted Message : "<<encrypted_message;
+
+  std::string decrypted_message = r1.decrypt_message(encrypted_message);
+  std::cout<<"Decrypted Message : "<<decrypted_message<<std::endl;
+
+  /*int opt;
+  ostream f1; 
+  while((opt = getopt(argc, argv, "n:t:")) != -1) {
+    switch(opt) {
+      case 'n' : 
+        std::string fname = std::string(optarg);
+        f1.open(fname);
+        if(!f1.is_open()){
+          std::cout<<fname << " : error"<<std::endl;
+        }
+        break;
+      
+      case 't' :
+        
+    }
+
+   
+  }*/

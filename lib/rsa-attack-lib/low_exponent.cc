75352bae61e5a3913679bc296b84757ea9fddc89
Christopher Findeisen <cfindeisen7@gmail.com>
2015-05-11 00:33:16 -0500

    Now prints results

--- lib/rsa-attack-lib/low_exponent.cc
+++ lib/rsa-attack-lib/low_exponent.cc
@@ -19,19 +19,16 @@ namespace lowexp {
     mpz_class t_1 = mpz_class(m_1.C) * (m_2.n * m_3.n) * rsatk::extended_euclidean(m_2.n * m_3.n, m_1.n)[0];
     mpz_class t_2 = mpz_class(m_2.C) * (m_1.n * m_3.n) * rsatk::extended_euclidean(m_1.n * m_3.n, m_2.n)[0];
     mpz_class t_3 = mpz_class(m_3.C) * (m_2.n * m_1.n) * rsatk::extended_euclidean(m_2.n * m_1.n, m_3.n)[0];
-
     mpz_class c = rsatk::mod((t_1 + t_2 + t_3), (m_1.n * m_2.n * m_3.n));
 
     mpz_class m;
     mpz_root(m.get_mpz_t(), c.get_mpz_t(), 3);
+    cout << "Decrypted message is: " << m << endl;
     return m;
   }
 
   bool lowexp_valid(rsatk::RSA_data m_1, rsatk::RSA_data m_2, rsatk::RSA_data m_3){
-    //if (m_1.C != m_2.C || m_1.C != m_3.C || m_1.C == -1) {
-    //cerr << "Invalid message to use low exponent attack. All three messages must be the same." << endl;
-    //return false;
-    //}
+
     if(m_1.e != m_2.e || m_1.e != m_3.e || m_1.e != 3){
       cerr << "Invalid message to use low exponent attack. Must have an exponent of 3." << endl;
       return false;
